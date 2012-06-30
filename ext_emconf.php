006795467cac4df73bc348d905686760f765d47c
Oliver Hader <oliver@typo3.org>
2012-06-30 15:19:21 +0000

    Initial ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "readspeaker".
+#
+# Auto generated 30-06-2012 16:52
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'ReadSpeaker - Online Text to Speech',
+	'description' => 'This extension allows to integrate the ReadSpeaker service (http://www.readspeaker.com/) into TYPO3.',
+	'category' => 'fe',
+	'author' => 'Oliver Hader',
+	'author_email' => 'oliver.hader@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"df3f";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"e7ad";s:20:"doc/wizard_form.html";s:4:"b448";}',
+);
+
+?>
