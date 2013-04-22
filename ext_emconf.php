3157f483b1c589bfc98064d0b10920e1f124a300
Oliver Hader <oliver@typo3.org>
2013-04-22 11:33:44 +0200

    [TASK] Update ext_emconf.php & ChangeLog

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "readspeaker".
  *
- * Auto generated 22-04-2013 09:48
+ * Auto generated 22-04-2013 11:33
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"9017";s:16:"ext_autoload.php";s:4:"f976";s:12:"ext_icon.gif";s:4:"6049";s:17:"ext_localconf.php";s:4:"8a32";s:14:"ext_tables.php";s:4:"2b25";s:14:"ext_tables.sql";s:4:"bb6c";s:13:"locallang.xml";s:4:"61f7";s:16:"locallang_db.xml";s:4:"1439";s:10:"README.txt";s:4:"86be";s:18:"Classes/Common.php";s:4:"f049";s:30:"Classes/Constants/Renderer.php";s:4:"5b53";s:34:"Classes/Hooks/PageRendererHook.php";s:4:"1bd5";s:36:"Classes/Services/AbstractService.php";s:4:"2b34";s:32:"Classes/Services/PageService.php";s:4:"f670";s:34:"Classes/Services/RenderService.php";s:4:"82b3";s:38:"Classes/Services/TypoScriptService.php";s:4:"262b";s:38:"Configuration/TypoScript/constants.txt";s:4:"b969";s:34:"Configuration/TypoScript/setup.txt";s:4:"072c";s:38:"Resources/Public/Images/basic1-117.png";s:4:"8bea";s:43:"Resources/Public/Images/icon_12px_black.gif";s:4:"5d6b";s:43:"Resources/Public/Images/icon_16px_black.gif";s:4:"6049";s:43:"Resources/Public/Images/sound_high_icon.png";s:4:"3582";s:44:"Resources/Public/Stylesheets/readspeaker.css";s:4:"37f1";s:14:"doc/manual.pdf";s:4:"83e4";s:14:"doc/manual.sxw";s:4:"e40b";}',
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"a25f";s:16:"ext_autoload.php";s:4:"f976";s:12:"ext_icon.gif";s:4:"6049";s:17:"ext_localconf.php";s:4:"8a32";s:14:"ext_tables.php";s:4:"2b25";s:14:"ext_tables.sql";s:4:"bb6c";s:13:"locallang.xml";s:4:"ee33";s:16:"locallang_db.xml";s:4:"1439";s:10:"README.txt";s:4:"86be";s:18:"Classes/Common.php";s:4:"f049";s:30:"Classes/Constants/Renderer.php";s:4:"5b53";s:34:"Classes/Hooks/PageRendererHook.php";s:4:"b7d2";s:36:"Classes/Services/AbstractService.php";s:4:"2b34";s:32:"Classes/Services/PageService.php";s:4:"f670";s:34:"Classes/Services/RenderService.php";s:4:"7883";s:38:"Classes/Services/TypoScriptService.php";s:4:"f0a7";s:38:"Configuration/TypoScript/constants.txt";s:4:"f9e8";s:34:"Configuration/TypoScript/setup.txt";s:4:"532d";s:38:"Resources/Public/Images/basic1-117.png";s:4:"8bea";s:43:"Resources/Public/Images/icon_12px_black.gif";s:4:"5d6b";s:43:"Resources/Public/Images/icon_16px_black.gif";s:4:"6049";s:43:"Resources/Public/Images/sound_high_icon.png";s:4:"3582";s:44:"Resources/Public/Stylesheets/readspeaker.css";s:4:"01bc";s:14:"doc/manual.pdf";s:4:"83e4";s:14:"doc/manual.sxw";s:4:"e40b";}',
 	'suggests' => array(
 	),
 );
