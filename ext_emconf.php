5244231cf22156bade061d372cb71ea23aff6659
Oliver Hader <oliver@typo3.org>
2013-11-08 10:59:34 +0100

    [TASK] Raise version to 0.3.1 (TYPO3 CMS 4.5.0-6.2.99)

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,10 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.3.0',
+	'version' => '0.3.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-0.0.0',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
